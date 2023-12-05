22a2216451f891da1e37392b70026aeb467d069c
PranitRout07 <pranitrout72@gmail.com>
2023-12-05 19:16:52 +0530

    Update check_ec2_instances.py

--- check_ec2_instances.py
+++ check_ec2_instances.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 from datetime import datetime, timezone, timedelta
-import time
+
 
 client = boto3.client('ec2')
 sns = boto3.client('sns')
@@ -43,8 +43,12 @@ def utilization(id):
         Statistics=['Average'],
     )
     # print(response, "------Response----")
-    time.sleep(80)
-    return response['Datapoints'][0]['Average']
+    # print((response['Datapoints']),"response",id)
+    if len(response['Datapoints'])==0:
+        return None
+    else:
+        return response['Datapoints'][0]['Average']
+    
 
 def running_instances():
     # create filter for instances in running state
@@ -91,17 +95,19 @@ def check_ec2_instance():
                     print("Utilization Response:", utilization_response)
                     print(" ")
                     average_utilization = utilization_response
-
-                    if average_utilization <= threshold_utilization:
-                        if time_difference >= max_threshold_time:
-                            sns_service(
-                                f"EC2 instance {instance_id} is underutilized and has crossed max threshold time. Terminating ec2 instance.")
-                            terminate_ec2_instance(instance_id)
-                        elif time_difference >= min_threshold_time and time_difference < max_threshold_time:
-                            sns_service(
-                                "Please take necessary actions. This instance has exceeded minimum threshold time and is still underutilized.")
-                        else:
-                            pass
+                    if average_utilization!=None:
+                        if average_utilization <= threshold_utilization:
+                            if time_difference >= max_threshold_time:
+                                sns_service(
+                                    f"EC2 instance {instance_id} is underutilized and has crossed max threshold time. Terminating ec2 instance.")
+                                terminate_ec2_instance(instance_id)
+                            elif time_difference >= min_threshold_time and time_difference < max_threshold_time:
+                                sns_service(
+                                    "Please take necessary actions. This instance has exceeded minimum threshold time and is still underutilized.")
+                            else:
+                                pass
+                    else:
+                        pass
                 except Exception as e:
                     print(f"Error processing instance {instance_id}: {e}")
                     print(" ")
