bec3788034e0499c7c5c8de24bf8cbbd7c917bb5
PranitRout07 <pranitrout72@gmail.com>
2023-12-07 07:14:13 +0530

    Update check_elastic_ips.py

--- check_elastic_ips.py
+++ check_elastic_ips.py
@@ -12,16 +12,20 @@ def lambda_handler(event, context):
     
     
     response = ec2.describe_addresses()
-    print(response)
-    print(" ")
-    print(" ")
-    print(" ")
+    not_associated_ips = []
     if len(response['Addresses'])!=0:
         id = response['Addresses'][0]['AllocationId']
-        if 'AssociationId' not in response['Addresses']:
-            print(f"Elatic IP {id} is released")
-            release_elastic_ips(id)
+        if 'AssociationId' not in response['Addresses'][0]:
+           
+            not_associated_ips.append(id)
         else:
+            print(id," : ",response['Addresses'][0]['AssociationId'])
             pass
     else:
         print("No elastic IPs present")
+    if len(not_associated_ips)!=0:
+        for id in not_associated_ips:
+                print(f"Elatic IP {id} is released")
+                release_elastic_ips(id)
+    else:
+        print("No unused elastic IPs")
